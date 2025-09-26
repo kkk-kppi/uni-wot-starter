82897102defd64d6f85808a67dc1c1df1c9df3c3
kkk-kppi <banban980924@163.com>
2025-09-26 18:30:00 +0800

    first commit

---
+++ src/pages/map/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+definePage({
+  name: 'map',
+  layout: 'tabbar',
+  style: {
+    navigationBarTitleText: '地图',
+  },
+})
+</script>
+
+<template>
+  <view class="min-h-screen bg-gray-100 py-3 dark:bg-[var(--wot-dark-background)]">
+    <map name="" />
+  </view>
+</template>
